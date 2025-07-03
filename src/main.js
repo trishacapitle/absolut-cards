0a138f38327a1f8e8d8445368b9abd818ad4be85
trishacapitle <140716826+trishacapitle@users.noreply.github.com>
2025-07-03 09:36:19 +0800

    Refactor animation functions for improved readability and consistency

--- src/main.js
+++ src/main.js
@@ -30,109 +30,114 @@ let isProgressBarVisible = false;
 let currentActiveIndex = -1;
 
 function animateIndexOpacity(newIndex) {
-  if (newIndex !== currentActiveIndex) {
-    indices.forEach((index, i) => {
-      gsap.to(index, {
-        opacity: i === newIndex ? 1 : 0.25,
-        duration: 0.5,
-        ease: "power2.Out"
-      })
-    });
+	if (newIndex !== currentActiveIndex) {
+		indices.forEach((index, i) => {
+			gsap.to(index, {
+				opacity: i === newIndex ? 1 : 0.25,
+				duration: 0.5,
+				ease: "power2.Out",
+			});
+		});
 
-    currentActiveIndex = newIndex;
-  }
+		currentActiveIndex = newIndex;
+	}
 }
 
 function showProgressAndIndices() {
-  gsap.to([progressBarContainer, indicesContainer], {
-    opacity: 1,
-    duration: 0.5,
-    ease: "power2.Out"
-  });
-isProgressBarVisible = true;
+	gsap.to([progressBarContainer, indicesContainer], {
+		opacity: 1,
+		duration: 0.5,
+		ease: "power2.Out",
+	});
+	isProgressBarVisible = true;
 }
 
 function hideProgressAndIndices() {
-  gsap.to([progressBarContainer, indicesContainer], {
-    opacity: 0,
-    duration: 0.5,
-    ease: "power2.Out"
-  });
-  isProgressBarVisible = false;
-  animateIndexOpacity(-1);
+	gsap.to([progressBarContainer, indicesContainer], {
+		opacity: 0,
+		duration: 0.5,
+		ease: "power2.Out",
+	});
+	isProgressBarVisible = false;
+	animateIndexOpacity(-1);
 }
 
 ScrollTrigger.create({
-  trigger: pinnedSection,
-  start: "top top",
-  end: `+=${pinnedHeight}`,
-  pin: true,
-  pinSpacing: true,
-  onLeave: () => {
-    hideProgressAndIndices();
-  },
-  onEnterBack: () => {
-    showProgressAndIndices();
-  },
-  onUpdate: (self) => {
-    const progress = self.progress * (cardCount + 1);
+	trigger: pinnedSection,
+	start: "top top",
+	end: `+=${pinnedHeight}`,
+	pin: true,
+	pinSpacing: true,
+	onLeave: () => {
+		hideProgressAndIndices();
+	},
+	onEnterBack: () => {
+		showProgressAndIndices();
+	},
+	onUpdate: (self) => {
+		const progress = self.progress * (cardCount + 1);
+		const currentCard = Math.floor(progress);
 
-    if (progress <= 1) {
-      gsap.to(stickyHeader, {
-        opacity: 1 - progress,
-        duration: 0.1,
-        ease: "none"
-      });
-    } else {
-      gsap.set(stickyHeader, {
-        opacity: 0
-      });
-    }
+		if (progress <= 1) {
+			gsap.to(stickyHeader, {
+				opacity: 1 - progress,
+				duration: 0.1,
+				ease: "none",
+			});
+		} else {
+			gsap.set(stickyHeader, {
+				opacity: 0,
+			});
+		}
 
-    if(progress > 1 && !isProgressBarVisible) {
-      showProgressAndIndices();
-    } else if (progress <= 1 && isProgressBarVisible) {
-      hideProgressAndIndices();
-    }
+		if (progress > 1 && !isProgressBarVisible) {
+			showProgressAndIndices();
+		} else if (progress <= 1 && isProgressBarVisible) {
+			hideProgressAndIndices();
+		}
 
-    let progressHeight = 0;
-    let colorIndex = -1;
-    if (progress > 1) {
-      progressHeight = ((progress - 1) / cardCount) * 100;
-      colorIndex = Math.min(Math.floor((progress - 1)), cardCount - 1);
-    }
+		let progressHeight = 0;
+		let colorIndex = -1;
+		if (progress > 1) {
+			progressHeight = ((progress - 1) / cardCount) * 100;
+			colorIndex = Math.min(Math.floor(progress - 1), cardCount - 1);
+		}
 
-    gsap.to(progressBar, {
-      height: `${progressHeight}%`,
-      backgroundColor: progressColors[colorIndex],
-      ease: "power1.Out",
-      duration: 0.3,
-    });
+		gsap.to(progressBar, {
+			height: `${progressHeight}%`,
+			backgroundColor: progressColors[colorIndex],
+			ease: "power1.Out",
+			duration: 0.3,
+		});
 
-    if (isProgressBarVisible) {
-      animateIndexOpacity(colorIndex);
-    }
+		if (isProgressBarVisible) {
+			animateIndexOpacity(colorIndex);
+		}
 
-    cards.forEach((card, index) => {
-      if (index < currentCard) {
-        gsap.set(card, {
-          rotation: endRotations[index],
-          top:"50%",
-        });
-      } else if (index === currentCard) {
-        const cardProgress = (progress - currentCard);
-        const newTop = gsap.utils.interpolate(150, 50, cardProgress);
-        const newRotation = gsap.utils.interpolate(startRotations[index], endRotations[index], cardProgress);
-        gsap.set(card, {
-          rotation: newRotation,
-          top: `${newTop}%`,
-        });
-      } else {
-        gsap.set(card, {
-          rotation: startRotations[index],
-          top: "150%",
-        });
-      }
-    })
-  }
+		cards.forEach((card, index) => {
+			if (index < currentCard) {
+				gsap.set(card, {
+					rotation: endRotations[index],
+					top: "50%",
+				});
+			} else if (index === currentCard) {
+				const cardProgress = progress - currentCard;
+				const newTop = gsap.utils.interpolate(150, 50, cardProgress);
+				const newRotation = gsap.utils.interpolate(
+					startRotations[index],
+					endRotations[index],
+					cardProgress
+				);
+				gsap.set(card, {
+					rotation: newRotation,
+					top: `${newTop}%`,
+				});
+			} else {
+				gsap.set(card, {
+					rotation: startRotations[index],
+					top: "150%",
+				});
+			}
+		});
+	},
 });
